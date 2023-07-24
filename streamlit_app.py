fec7c202ff6656f4dc68429234ca66e573c9b604
i.shutikhin <shutikhin.igor@yandex.ru>
2023-07-24 17:02:27 +0500

    clone chat gpt

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,32 @@ with st.chat_message("assistant"):
     st.write("new chart")
     st.bar_chart(np.random.randn(30, 3))
 
+st.title("Echo Bot")
 
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+'''
 st.title("ChatGPT-like clone")
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
@@ -59,3 +84,4 @@ if prompt := st.chat_input("What is up?"):
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
+'''
