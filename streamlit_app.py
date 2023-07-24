02f94ccadb8246ab2b5e0a05a001a0b926b6d5f4
i.shutikhin <shutikhin.igor@yandex.ru>
2023-07-24 16:32:20 +0500

    new input and bot

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,36 @@ for row in df.itertuples():
 with st.chat_message("assistant"):
     st.write("new chart")
     st.bar_chart(np.random.randn(30, 3))
+
+#new prompt
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+
+
+st.title("Echo Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+
+response = f"Echo: {prompt}"
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    st.markdown(response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
